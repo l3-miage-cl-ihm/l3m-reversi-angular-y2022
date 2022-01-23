768af9c7d6c2b6ec64da5d61866956db976a2200
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2022-01-23 22:28:47 +0100

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IaService } from './ia.service';
 import { ReversiGameEngineService } from './reversi-game-engine.service';
+import { Board_RO, C } from './ReversiDefinitions';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,35 @@ export class AppComponent {
 
   constructor(public RGS: ReversiGameEngineService, private ia: IaService) {}
 
+  get board(): Board_RO {
+    return this.RGS.board;
+  }
+
+  symboleFor(c: C): string {
+    switch (c) {
+      case 'Player1': return "X";
+      case 'Player2': return "O";
+      default: return "";
+    }
+  }
+
+  play(i: number, j: number): void {
+    this.RGS.play(i, j);
+  }
+
+  get score(): string {
+    let nb1 = 0;
+    let nb2 = 0;
+    this.RGS.board.forEach( L => L.forEach( c => {
+      switch (c) {
+        case 'Player1': nb1++; break;
+        case 'Player2': nb2++; break;
+      }
+    }) );
+
+    const t = this.RGS.turn;
+
+    return `${t === 'Player1' ? '-->': '   '}Player 1 (X) : ${nb1}
+${t === 'Player2' ? '-->': '   '}Player 2 (O) : ${nb2}`
+  }
 }
