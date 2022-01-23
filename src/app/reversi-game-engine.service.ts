81591d34bf5cf50f4d4f7f6f01e780abbcc7fd69
Alexandre Demeure <alexandre.demeure@univ-grenoble-alpes.fr>
2022-01-23 22:29:40 +0100

    ngZone support from console

--- src/app/reversi-game-engine.service.ts
+++ src/app/reversi-game-engine.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Injectable, NgZone } from '@angular/core';
+import { BehaviorSubject, Observable, OperatorFunction, share } from 'rxjs';
 import { Board, BoardtoString, Board_RO, C, charToTurn, GameState, getEmptyBoard, PlayImpact, ReversiModelInterface, TileCoords, Turn } from './ReversiDefinitions';
+
+export function runInZone<T>(zone: NgZone): OperatorFunction<T, T> {
+  return (source) => {
+    return new Observable(observer => {
+      const next     = (value: T)   => zone.run(() => observer.next(value));
+      const error    = (e: unknown) => zone.run(() => observer.error(e)   );
+      const complete = ()           => zone.run(() => observer.complete() );
+      return source.subscribe({next, error, complete});
+    });
+  };
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +22,16 @@ export class ReversiGameEngineService implements ReversiModelInterface {
     board: getEmptyBoard(),
     turn: 'Player1'
   });
-  public readonly gameStateObs: Observable<GameState> = this.gameStateSubj.asObservable();
+  public readonly gameStateObs: Observable<GameState>;
 
   // NE PAS MODIFIER
-  constructor() {
+  constructor(private ngz: NgZone) {
+      this.gameStateObs = this.gameStateSubj.asObservable().pipe(
+        runInZone(ngz),
+        share({
+          connector: () => new BehaviorSubject( this.gameStateSubj.value )
+        })
+      );
       this.restart();
       // NE PAS TOUCHER, POUR LE DEBUG DANS LA CONSOLE
       (window as any).RGS = this;
